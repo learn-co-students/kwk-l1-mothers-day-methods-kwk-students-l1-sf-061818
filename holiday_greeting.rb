d632759c9b56dd2bc691670ea11a298a6469a084
Clarissa Wu <clarissa.swu@gmail.com>
2018-06-19 17:41:02 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,6 @@
+def holiday_greeting(name1="Mom", name2 = "Favourite child", holiday="Mother's Day")
+  puts "Happy #{holiday}, #{name1}! - From #{name2}"
+end
+
+holiday_greeting("Beyonce","Jay-z","Fourth of July")
+holiday_greeting
--- mothers_day.rb
+++ mothers_day.rb
@@ -1,3 +1,15 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+def mothers_day
+  puts "Happy Mother's Day, Mom!"
+end
+mothers_day
+
+def mothers_day(mom_name="Mom")
+  puts "Happy Mother's Day, #{mom_name}!"
+end
+mothers_day("Beyonce")
+
+
+
 
